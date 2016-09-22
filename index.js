22dd17d0bdb06f67cc9934218efcc729d600327d
Schyler Hosler <schylerhosler@gmail.com>
2016-09-22 16:22:43 -0400

    Initial commit: React.js practice with use of variables and rendering -- All tests passing

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 //Code React element here
+// var meInReact = (
+  // <div class="me">
+  //   <h1>An Awesome Person</h1>
+  //   <p>Who is learning React</p>
+  //
+    // <ul class="me__interests">
+    //   <li>JavaScript</li>
+    //   <li>React</li>
+    //   <li>Movies</li>
+    //   <li>Ice cream</li>
+    // </ul>
+  // </div>
+// );
+const javaScriptLi = React.createElement('li', {}, 'JavaScript')
+const reactLi = React.createElement('li', {}, 'React')
+const moviesLi = React.createElement('li', {}, 'Movies')
+const iceCreamnLi = React.createElement('li', {}, 'Ice cream')
+const myUl = React.createElement('ul', {}, [javaScriptLi, reactLi, moviesLi, iceCreamnLi])
+const myP = React.createElement('p', {}, 'Who is learning React')
+const myH1 = React.createElement('h1', {}, 'An Awesome Person')
+const meInReact = React.createElement('div', {className: "me"}, [myH1, myP, myUl])
+
+ReactDOM.render(
+  meInReact,
+  document.getElementById('main')
+);
+// ReactDOM.render(meInReact, document.getElementById('main'));
